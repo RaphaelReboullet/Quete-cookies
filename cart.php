80a01dedd0cf9ce0cfdef65ec8f65fa9d9008c7b
Raphael Reboullet <raphaelreboullet@gmail.com>
2018-10-23 09:24:07 +0200

    hxu

---
+++ cart.php
@@ -0,0 +1,34 @@
+<?php require 'inc/head.php';
+if (!isset($_SESSION['loginname'])){
+    header ('location: login.php');
+}
+?>
+
+
+<section class="cookies container-fluid">
+    <div class="container justify content-center">
+    <div class="row">
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Cookie</th>
+
+
+            </tr>
+            </thead>
+            <tbody>
+<?php foreach ($_SESSION['cart']['name'] as $value => $key) { ?>
+    <tr>
+                <th scope="row"><?php echo $key ?></th>
+            </tr>
+<?php }; ?>
+
+
+            </tbody>
+        </table>
+
+    </div>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php require 'inc/head.php';
+
+if (!isset($_SESSION['loginname']))
+{
+    header ('location: login.php');
+}
+
+$cart = [];
+
+if (isset($_GET['name']))
+{
+    $_SESSION ['cart']['name'][] = $_GET['name'];
+}
+
+
+
+?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Pecan nuts</h3>
+                    <p>Cooked by Penny !</p>
+                    <a href="?ref=46&name=pecan" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                    </a>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Chocolate chips</h3>
+                    <p>Cooked by Bernadette !</p>
+                    <a href="?ref=36&name=chocolatechips" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                    </a>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>Chocolate cookie</h3>
+                    <p>Cooked by Bernadette !</p>
+                    <a href="?ref=58&name=chocolatecookie" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                    </a>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <figure class="thumbnail text-center">
+                <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+                <figcaption class="caption">
+                    <h3>M&M's&copy; cookies</h3>
+                    <p>Cooked by Penny !</p>
+                    <a href="?ref=32&name=mms" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                    </a>
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
